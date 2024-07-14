49a7a0c8139a72611072c4b04d5776907cee9edb
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-07-14 19:50:57 +0530

    Add paddle, background and movement in the bricksmash game

--- BrickSmash/src/App.java
+++ BrickSmash/src/App.java
@@ -4,10 +4,12 @@ import javax.swing.JFrame;
 public class App {
     public static void main(String[] args){
         JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Breakout_Ball");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
     }
 }
--- BrickSmash/src/Gameplay.java
+++ BrickSmash/src/Gameplay.java
@@ -1,13 +1,13 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
-
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener{
     private boolean play = false;
@@ -39,30 +39,75 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
         g.fillRect(1, 1, 692, 592);
 
         //borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        //the paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+        
+        //the ball
+        g.setColor(Color.yellow);
+        g.fillOval(ballposX, ballposY, 20, 20);
+        g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        timer.start();
+        if(play){
+            if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))){
+                ballYdir = -ballYdir;
+            }
+            ballposX += ballXdir;
+            ballposY += ballYdir;
+            if(ballposX < 0){
+                ballXdir = -ballXdir;
+            }
+            if(ballposY < 0){
+                ballYdir = -ballYdir;
+            }
+            if(ballposX < 670){
+                ballXdir = -ballXdir;
+            }
+            
+        }
+
+        repaint();
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
-    }
+    public void keyTyped(KeyEvent e) {}
 
+    @Override
+    public void keyReleased(KeyEvent e) {}
+    
     @Override
     public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if(playerX >= 600){
+                playerX = 600;
+            } else{
+                moveRight();
+            }
+        }
+    if(e.getKeyCode() == KeyEvent.VK_LEFT){
+        if(playerX < 10){
+            playerX = 10;
+        } else{
+            moveLeft();
+            }
+        }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    public void moveRight(){
+        play = true;
+        playerX += 20;
     }
-
+    public void moveLeft(){
+        play = true;
+        playerX -= 20;
+    }
 }
