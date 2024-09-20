cbb2bed86aec471b7041394605bb857c8be40a57
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-09-20 21:24:50 +0530

    create .gitignore

---
+++ Flappy bird/src/FlappyBird.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel{
+    int boardwidth = 340;
+    int boardheight = 640;
+
+    public FlappyBird() {
+    }
+
+    
+}
