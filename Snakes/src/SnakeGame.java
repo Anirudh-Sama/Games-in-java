6654b86a6783d181c8b0d9b48c148481f9d30b9d
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-09-26 21:06:17 +0530

    Create a .gitignore file, snake and tilesize

--- Snakes/src/SnakeGame.java
+++ Snakes/src/SnakeGame.java
@@ -1,15 +1,40 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel{
+    private class Tile{
+        int x;
+        int y;
+        Tile(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
+
+    Tile snakeHead;
 
     SnakeGame(int boardWidth, int boardHeight){
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
+
+        snakeHead = new Tile(5, 5);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //Snake
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x*tileSize, snakeHead.y*tileSize, tileSize, tileSize);
     }
 }
